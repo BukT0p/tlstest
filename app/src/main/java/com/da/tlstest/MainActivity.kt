54523d6a96767deec2c112cde4142bf0da820321
vyakunin <Victor.Yakunin@dataart.com>
2020-05-20 23:31:24 +0300

    Final commit

--- app/src/main/java/com/da/tlstest/MainActivity.kt
+++ app/src/main/java/com/da/tlstest/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         webView.loadUrl(BASE_URL)
         lifecycleScope.launch {
-            text.text = checkTLS()
+            text.append(checkTLS())
         }
     }
 
