950b59e54be2cca5c7751d0fe5da95d80a893f28
vyakunin <Victor.Yakunin@dataart.com>
2020-05-20 23:22:16 +0300

    Clean up

--- app/src/main/java/com/da/tlstest/AppClass.kt
+++ app/src/main/java/com/da/tlstest/AppClass.kt
@@ -1,19 +1,12 @@
 package com.da.tlstest
 
 import android.app.Application
-import android.os.Build
 import org.conscrypt.Conscrypt
-import org.conscrypt.OpenSSLProvider
 import java.security.Security
 
 class AppClass : Application() {
-
     override fun onCreate() {
         super.onCreate()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Security.insertProviderAt(Conscrypt.newProvider(), 1)
-        } else {
-            Security.insertProviderAt(OpenSSLProvider("OkHttp"), 1)
-        }
+        Security.insertProviderAt(Conscrypt.newProvider(), 1)
     }
 }
--- app/src/main/java/com/da/tlstest/MainActivity.kt
+++ app/src/main/java/com/da/tlstest/MainActivity.kt
@@ -7,10 +7,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
-import okhttp3.logging.HttpLoggingInterceptor
 import org.jsoup.Jsoup
 import retrofit2.Retrofit
 import retrofit2.http.GET
@@ -19,13 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private val retrofit: IApi by lazy {
         Retrofit.Builder()
-            .client(
-                OkHttpClient.Builder()
-                    .connectionSpecs(listOf(ConnectionSpec.MODERN_TLS))
-                    .addInterceptor(HttpLoggingInterceptor().apply {
-                        this.level = (HttpLoggingInterceptor.Level.BODY)
-                    }).build()
-            )
+            .client(OkHttpClient.Builder().build())
             .baseUrl(BASE_URL)
             .build()
             .create(IApi::class.java)
@@ -34,19 +26,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            lifecycleScope.launch {
-                text.text = testTLS(retrofit.check())
-            }
-        }
         webView.loadUrl(BASE_URL)
+        lifecycleScope.launch {
+            text.text = checkTLS()
+        }
     }
 
-    private suspend fun testTLS(responseBody: ResponseBody) = withContext(Dispatchers.IO) {
+    private suspend fun checkTLS(): String = withContext(Dispatchers.IO) {
+        val responseBody = retrofit.check()
         val dom = Jsoup.parse(responseBody.byteStream(), null, BASE_URL)
         dom.body().getElementsByTag("div").firstOrNull {
             it.children().first().text() == "Version"
-        }?.children()?.get(1)?.text()
+        }?.children()?.get(1)?.text() ?: "DOM changed, full body:\n\n${dom.body().text()}"
     }
 
     interface IApi {
